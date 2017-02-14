80ae1b4cf8275e53aea8ee695a7da819166da657
Shuhei Nomura <info@danmaq.com>
2017-02-14 13:03:24 +0900

    Windows版ローカルテストを実装。

--- test/run.cmd
+++ test/run.cmd
@@ -6,15 +6,19 @@ echo Docker service and client are required to run this setup.
 exit /b 1
 :PRE_STEP1
 
+if not exist "_site\" md _site
+
 setlocal
 set VSRC=%~dp0
 set CONTAINER=jekyll
 set IMAGE=danmaq/jekyll-git-redcarpet
-docker run -d -p 4000:4000 -v %VSRC:~0,-1%:/v/:ro --name %CONTAINER% %IMAGE% sh -c "mkdir /root/.ssh;while true; do sleep 1; done"
+set VOL1=%VSRC:~0,-6%:/v/:ro
+set VOL2=%VSRC:~0,-5%_site:/srv/jekyll/_site
+set LOOP="mkdir /root/.ssh; while true; do sleep 1; done"
+docker run -d -p 4000:4000 -v %VOL1% -v %VOL2% --name %CONTAINER% %IMAGE% sh -c %LOOP%
 echo "Please open via browser: http://localhost:4000"
-
 docker exec -it %CONTAINER% /v/test/incontainer
-docker exec -it %CONTAINER% ash
+rem docker exec -it %CONTAINER% ash
 docker rm -f %CONTAINER%
 endlocal
 exit /b 0
