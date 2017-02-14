7432ae195e7abac70391e748f21eb8c3bb75e302
Shuhei Nomura <info@danmaq.com>
2017-02-14 10:49:55 +0900

    フォルダ配置整理。

--- test/run.cmd
+++ test/run.cmd
@@ -2,18 +2,18 @@
 
 where /Q docker
 if errorlevel 0 goto PRE_STEP1
-echo Docker is required to run this setup.
+echo Docker service and client are required to run this setup.
 exit /b 1
 :PRE_STEP1
 
 setlocal
+set VSRC=%~dp0
 set CONTAINER=jekyll
 set IMAGE=danmaq/jekyll-git-redcarpet
-docker build -t %IMAGE% _test
-docker run -d -p 4000:4000 -v %~dp0:/v/:ro --name %CONTAINER% %IMAGE% sh -c "mkdir /root/.ssh;while true; do sleep 1; done"
+docker run -d -p 4000:4000 -v %VSRC:~0,-1%:/v/:ro --name %CONTAINER% %IMAGE% sh -c "mkdir /root/.ssh;while true; do sleep 1; done"
 echo "Please open via browser: http://localhost:4000"
 
-docker exec -it %CONTAINER% /v/test.incontainer
+docker exec -it %CONTAINER% /v/test/incontainer
 docker exec -it %CONTAINER% ash
 docker rm -f %CONTAINER%
 endlocal
