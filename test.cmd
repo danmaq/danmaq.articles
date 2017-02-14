0aa100b0897e0a5764e8f3727dea15d71315fd4a
Shuhei Nomura <info@danmaq.com>
2017-02-14 09:26:12 +0900

    バッチの漏れ修正

--- test.cmd
+++ test.cmd
@@ -9,11 +9,12 @@ exit /b 1
 setlocal
 set CONTAINER=jekyll
 set IMAGE=danmaq/jekyll-git-redcarpet
-docker run -d -p 4000:4000 -v %~dp0:/v:ro --name %CONTAINER% %IMAGE% sh -c "mkdir /root/.ssh;while true; do sleep 1; done"
+docker build -t %IMAGE% _test
+docker run -d -p 4000:4000 -v %~dp0:/v/:ro --name %CONTAINER% %IMAGE% sh -c "mkdir /root/.ssh;while true; do sleep 1; done"
 echo "Please open via browser: http://localhost:4000"
 
 docker exec -it %CONTAINER% /v/test.incontainer
-rem docker exec -it %CONTAINER% ash
+docker exec -it %CONTAINER% ash
 docker rm -f %CONTAINER%
 endlocal
 exit /b 0
