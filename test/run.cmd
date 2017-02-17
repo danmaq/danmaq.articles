52a655227018059cb2f0c3e7351f4223fb60d0b6
Shuhei Nomura <info@danmaq.com>
2017-02-17 13:03:34 +0900

    秘密鍵を共有ではなくコピーで渡すようにした。

--- test/run.cmd
+++ test/run.cmd
@@ -27,23 +27,19 @@ set VOL2=%PJHOME%:%DEST%:ro
 rem ==== SET VOL3 ====
 set VOL3=%PJHOME%\_site:/srv/jekyll/_site
 
-rem ==== SET VOL4 ====
-set VOL4=%USERPROFILE%\.ssh\id_rsa:/root/.ssh/id_rsa:ro
-
 set CONTAINER=jekyll
 set IMAGE=danmaq/jekyll-git-redcarpet:github-pages
 set GIT_NAME="jekyll bot by Shuhei Nomura"
 set GIT_EMAIL="info@danmaq.com"
 set LOOP="while true; do sleep 1; done"
-
 docker run -d -p 4000:4000 ^
-    -v %VOL1% -v %VOL2% -v %VOL3% -v %VOL4% ^
+    -v %VOL1% -v %VOL2% -v %VOL3% ^
     -e GIT_NAME=%GIT_NAME% -e GIT_EMAIL=%GIT_EMAIL% ^
     --name %CONTAINER% %IMAGE% sh -c %LOOP%
 echo "Please open via browser: http://localhost:4000"
-
+docker cp %USERPROFILE%\.ssh\id_rsa %CONTAINER%:/root/.ssh/id_rsa
 docker exec -it %CONTAINER% %DEST%/test/incontainer
-rem docker exec -it %CONTAINER% ash
+docker exec -it %CONTAINER% ash
 docker rm -f %CONTAINER%
 endlocal
 exit /b 0
